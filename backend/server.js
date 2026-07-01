af40a2c0fe6cbed2d4d8ca7df7eba5278b9322fb
Tanvi Sharma <tanvis136@gmail.com>
2026-07-01 13:46:43 +0530

    fix deployment port

--- backend/server.js
+++ backend/server.js
@@ -67,14 +67,16 @@ io.on("connection", (socket) => {
 // 🔥 THIS IS THE FIX (was initSocket)
 socketHandler(io);
 
+const PORT = process.env.PORT || 5000;
 
 // DB + server start
 mongoose
   .connect(process.env.MONGO_URI)
   .then(() => {
-    console.log("MongoDB connected");
-    server.listen(5000, () =>
-      console.log("Server running on 5000")
+    console.log("MongoDB connected"); 
+    
+    server.listen(PORT, () =>
+      console.log("Server running on",PORT)
     );
   })
   .catch(console.log);
